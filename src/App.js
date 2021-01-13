bdbc2e5bd073b0dfaa93fbfba1e32bc219dc23fc
Subhadip <subhadip@authmetrik.com>
2021-01-13 15:37:54 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {CardList} from "./Components/card-list/card-list.component";
+import {SearchBox} from "./Components/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            monsters : [],
+            searchField : ''
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then( response => response.json())
+            .then( users => this.setState({ monsters : users }))
+    }
+
+    search() {
+        return this.state.monsters.filter(monster =>
+            monster.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        )
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <h1>Monsters Rolodex</h1>
+                <SearchBox
+                    for='search monster'
+                    handleChange={e => this.setState({searchField : e.target.value})}
+                />
+                <CardList monsters={this.search()} />
+            </div>
+        );
+    }
+
 }
-
-export default App;
---
+++ src/Components/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import  React from "react";
+import './search-box.styles.css';
+
+export const SearchBox = props => (
+    <input
+        className='search'
+        type='search'
+        placeholder={props.for}
+        onChange={props.handleChange} />
+)
